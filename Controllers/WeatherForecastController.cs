7843bed7f40f1d1c3f2b0eded4115f1266cc9845
Ryan Rowston <Ryan.Rowston@taptu.com.au>
2024-02-12 08:17:54 +1030

    Set up unrelaible API endpoints.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ public class WeatherForecastController : ControllerBase
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
+    private static int RequestCount = 0;
     private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -18,8 +18,8 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet("reliable")]
+    public IEnumerable<WeatherForecast> GetReliable()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
@@ -28,5 +28,53 @@ public class WeatherForecastController : ControllerBase
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
+    }
+
+    [HttpGet("unreliable")]
+    public IEnumerable<WeatherForecast> GetUnreliable()
+    {
+        RequestCount++;
+
+        // Only succeed every 3 attempts
+        if (RequestCount % 3 == 0)
+        {
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        else
+        {
+            throw new Exception("I'm Unreliable :p");
+        }
+    }
+
+    [HttpGet("borked")]
+    public IActionResult GetBorked()
+    {
+        throw new NotImplementedException("I'm VERY Unreliable");
+    }
+
+    [HttpGet("Slow")]
+    public async Task<IEnumerable<WeatherForecast>> GetSlowAsync()
+    {
+        RequestCount++;
+
+        // Be very slow every other attempt
+        if (RequestCount % 2 == 0)
+        {
+            await Task.Delay(15000);
+        }
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        }).ToArray();
     }
 }
